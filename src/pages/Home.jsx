cc9e9959752fb6b9d429f231a46a170b77d8cf2a
Nicholas Thomson <nicholasgraythomson@gmail.com>
2026-01-11 14:10:30 -0500

    update homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         <img src="/images/cherries.jpeg" alt="Cherries painting" />
         <img src="/images/goop-jars.jpeg" alt="Goop jars painting" />
         <img src="/images/nt.jpeg" alt="nt" /> 
-        <img src="/images/doorway-1.jpg" alt="doorway" />
+        <img src="/images/granny-1.jpeg" alt="granny" />
       </div>
     </section>
   );
